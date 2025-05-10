a24fcc30d86c4bf8ee78542de4b0de0c9e836936
momo391 <ia7636hs@gmail.com>
2025-05-10 14:24:38 +0100

    feat: refactor MongoDB connection to use a new mongo.ts file

--- src/lib/server/db.ts
+++
@@ -1,7 +0,0 @@
-import { env } from "$env/dynamic/private";
-import { MongoClient } from "mongodb";
-
-const client = new MongoClient(env.MONGODB_URI);
-const db = client.db();
-
-export { db };
---
+++ src/lib/server/mongo.ts
@@ -0,0 +1,23 @@
+// src/lib/server/mongo.ts
+import { env } from "$env/dynamic/private";
+import { MongoClient, type MongoClientOptions } from "mongodb";
+
+const uri = env.MONGODB_URI!;
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!globalThis._mongoClientPromise) {
+  client = new MongoClient(uri);
+  globalThis._mongoClientPromise = client.connect();
+}
+clientPromise = globalThis._mongoClientPromise;
+
+export async function getDb() {
+  const client = await clientPromise;
+  return client.db("svelte-auth");
+}
+
+declare global {
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
